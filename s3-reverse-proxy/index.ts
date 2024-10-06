70b20b33c5c1170718c5f68c0e127175d7ae8eb6
Prayag <prayagbhatt2003@gmail.com>
2024-10-06 18:31:11 +0530

    feat(reverse-proxy): reverse proxy logic

---
+++ s3-reverse-proxy/index.ts
@@ -0,0 +1,28 @@
+import * as dotenv from "dotenv";
+import { Request, Response } from "express";
+const express = require("express");
+const httpProxy = require("http-proxy");
+
+dotenv.config();
+const app = express();
+const PORT = 8000;
+const BASE_PATH = `https://output-vercel-clone-bucket.s3.ap-south-1.amazonaws.com/__outputs`
+
+const proxy = httpProxy.createProxyServer()
+
+app.use((req: Request, res: Response) => {
+    const hostname = req.hostname;
+    const subdomain = hostname.split('.')[0];
+    const resolvesTo = `${BASE_PATH}/${subdomain}`;
+
+    return proxy.web(req, res, { target: resolvesTo, changeOrigin: true }, (err: any) => {
+        if (err) {
+            console.error('Proxy error:', err);
+            res.status(500).send('Proxy error');
+        }
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(`Reverse Proxy Server is running on port ${PORT}`);
+});
